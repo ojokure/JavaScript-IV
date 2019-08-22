4e903ee1f5a4babdcb80611fd6fb0546300b25a8
ojokure <ojokuredim@gmail.com>
2019-08-22 14:50:52 +0100

    created Instructor an extension of person

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,22 @@
 // CODE here for your Lambda Classes
+
+class Person {
+    constructor(name, age, location, ) {
+        this.name = name;
+        this.age = age;
+        this.location = location;
+    }
+
+    speak() {
+        return `Hello my name is ${this.name}, I am from ${location}`
+    }
+}
+
+class Instructor extends Person {
+  constructor(name,age,location,specialty,favLanguage,catchPhrase){
+      super(name,age);
+  }
+
+
+}
+
